42d793921c849b35f11d4bf39c79b3ef0792b467
MyFirstAndLastUsername <51070691+MyFirstAndLastUsername@users.noreply.github.com>
2019-06-01 10:53:35 +0700

    Animation + Scripting
    +Animation: 2+ Objects
    +Scripting: Board candy crush

---
+++ A02/Assets/Scripts/Instantiate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate : MonoBehaviour {
+    public List<GameObject> Objects = new List<GameObject>(6);
+    // Use this for initialization
+    void Start () {
+        float x, y;
+        x = -11f; y = -4f;
+        float nextObjects = 2f;
+        for (int i = 0; i < 12; ++i)
+        {
+            for (int j = 0; j < 5; ++j)
+            {
+                int id_object = Random.Range(0, 6);
+                Vector3 scale = new Vector3(5f, 5f, 0);
+                Instantiate(Objects[id_object], new Vector3(x + nextObjects * i, y + nextObjects * j, 5), Quaternion.identity);
+            }
+        }   
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
